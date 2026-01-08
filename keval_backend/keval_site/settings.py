b45d68eef6f5ceaa4f43a05e6625e228faa22895
kaskazini <kaskazinisolutions@gmail.com>
2026-01-09 00:47:52 +0300

    white noise remove

--- keval_backend/keval_site/settings.py
+++ keval_backend/keval_site/settings.py
@@ -73,7 +73,7 @@ AUTH_USER_MODEL = 'accounts.CustomUser'
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware', # 
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',  # 
+    # 'whitenoise.middleware.WhiteNoiseMiddleware',  # 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
