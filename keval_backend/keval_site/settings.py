93fb59d44390cb9f7ab8b4eea5f70fc9d4e93ef7
kaskazini <kaskazinisolutions@gmail.com>
2026-02-02 23:44:22 +0300

    super user in settings

--- keval_backend/keval_site/settings.py
+++ keval_backend/keval_site/settings.py
@@ -74,6 +74,17 @@ INSTALLED_APPS = [
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
 
+# Django REST Framework settings
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ],
+}
+
 
 
 MIDDLEWARE = [
